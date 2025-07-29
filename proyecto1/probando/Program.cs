d2b96ba4ef903dabab26251609289524c6d4cc0e
jefersonluna520 <jefersonlungd@gmail.com>
2025-07-28 19:42:15 -0500

    cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Platzi!");
 
 string stripe_api_key="sk_test_4e5f8b2c-3d6f-4c1a-9b2e-7f8c9d0e1f2g";
+
+string simulatedConnectionString = "Server=localhost;Database=SimulacionDB;User Id=demoUser;Password=demoPass;";
